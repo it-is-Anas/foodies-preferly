679672d7631868c0c46735d1649769d0d3ca76e9
prontoTheLegend <anasprog8@gmail.com>
2025-09-04 13:04:25 +0300

    finished the meal page ,meal grid and meal item ui

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,19 +1,64 @@
 import MealGird from "@/components/meals/meal-grid";
 import Link from "next/link";
 
-import meals  from '../../faker/mealsFaker.ts';
+import food1 from '../../assets/food1.png';
 
 export default function MealsPage(){
+
+
+    const meals:{id:number,title:string,slug:string,image: any ,summary: string ,creator:string }[] = [
+        {
+            id: 1,
+            title: 'Donatelos choclate!',
+            slug: 'xchjk',
+            image: food1,
+            summary: 'a delicouse food here you can see delicouse food here you can see delicouse food here you can see ...',
+            creator: 'Me',
+        },
+        {
+            id: 133,
+            title: 'Donatelos choclate!',
+            slug: 'xchjk',
+            image: food1,
+            summary: 'a delicouse food here you can see delicouse food here you can see delicouse food here you can see ...',
+            creator: 'Me',
+        },
+        {
+            id: 21,
+            title: 'Donatelos choclate!',
+            slug: 'xchjk',
+            image: food1,
+            summary: 'a delicouse food here you can see delicouse food here you can see delicouse food here you can see ...',
+            creator: 'Me',
+        },
+        {
+            id: 13,
+            title: 'Donatelos choclate!',
+            slug: 'xchjk',
+            image: food1,
+            summary: 'a delicouse food here you can see delicouse food here you can see delicouse food here you can see ...',
+            creator: 'Me',
+        },
+        {
+            id: 11,
+            title: 'Donatelos choclate!',
+            slug: 'xchjk',
+            image: food1,
+            summary: 'a delicouse food here you can see delicouse food here you can see delicouse food here you can see ...',
+            creator: 'Me',
+        },
+    ];
+
     return (
         <>
-            <header className="w-[80%] mx-[auto] h-[15em] bg-[red]">
-                <h1 className="">Delecious meals , created{' '}<span className="">by you</span> </h1>
-                <p className="">chose your favorate recipe and cook it your self , it is easy and fun !</p>
-                <p className="">
+            <header className="w-[80%] mx-[auto] h-[15em] flex flex-col justify-center items-center ">
+                <h1 className="text-[2em] py-[10px] text-center font-[600] text-[#2f2f2f]">Delecious meals , created{' '}<span className="">by you</span> </h1>
+                <p className="text-[1.2em] py-[10px] text-center font-[600] text-[#2f2f2f]">chose your favorate recipe and cook it your self , it is easy and fun !</p>
+                <p className="text-[1.2em] py-[10px] text-center font-[600] text-[#2f2f2f]">
                     <Link href='/meals/share' className="" >share your favorate recipe</Link>
                 </p>
             </header>
-            <main className="w-[80%] mx-[auto] h-[15em] bg-[green]">
+            <main className="w-[80%] mx-[auto]">
                 <MealGird meals={meals} />
             </main>
         </>
--- components/meals/meal-item.tsx
+++ components/meals/meal-item.tsx
@@ -2,20 +2,20 @@ import Image  from 'next/image';
 import Link from 'next/link';
 
 export default function MealItem({title,slug,image,summary,creator}:
-    {title:string,slug:string,image: string ,summary: string ,creator:string }
+    {id: number,title:string,slug:string,image: any ,summary: string ,creator:string }
 ){
     return (
-        <article className="w-[15em]  bg-[yellow]">
-            <header className="w-[100%] h-[10em] bg-[purple]">
-                <Image src={image} alt={title}  className='w-[100%] h-[100%]'  ></Image>
-                <div className="">
-                    <h2 className=""> { title } </h2>
-                    <p className="">by { creator } </p>
+        <article className="w-[20em] p-[10px] m-[4px] rounded-[10px] bg-[#2f2f2f] text-[#f2f2f2]">
+            <header className="w-[100%] h-[100%] flex flex-col justify-start items-start ">
+                <Image src={image} alt={title}  className='w-[20em] h-[15em]'  ></Image>
+                <div className=" mr-[auto] flex flex-col justify-start items-start">
+                    <h2 className="font-[600] text-[1.4em]"> { title } </h2>
+                    <p className="text-[.8em]">by { creator } </p>
                 </div>
             </header>
-            <div className="">
-                <p className="">{ summary }</p>
-                <div className="">
+            <div className="flex flex-col justify-start items-start">
+                <p className="font-[400] text-[.8em]">{ summary }</p>
+                <div className="bg-[orange] my-[5px] px-[10px] py-[5px] ml-[auto] rounded-[10px]">
                     <Link href={`/meals/${slug}`} >View Details</Link>
                 </div>
             </div>
