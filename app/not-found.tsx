7e22bc6ddd71cfc054e2b14724831b25c51a7260
prontoTheLegend <anasprog8@gmail.com>
2025-09-06 10:01:03 +0300

    not found page added

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+
+
+export default function NotFoundPage(){
+    return(
+        <main className="w-[100%] h-[50vh] flex flex-col justify-center items-center">
+            <h1 className="font-[600] text-[2em]" >Not found anything 404!</h1>
+        </main>
+    );
+}
