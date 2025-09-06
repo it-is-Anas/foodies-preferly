70b4678d8b6d46a80388aa8d5e314a34652d3389
prontoTheLegend <anasprog8@gmail.com>
2025-09-06 10:47:24 +0300

    adding not found to meal-[slug] page

---
+++ app/meals/[slug]/not-found.tsx
@@ -0,0 +1,9 @@
+
+
+export default function NotFoundPage(){
+    return(
+        <main className="w-[100%] h-[50vh] flex flex-col justify-center items-center">
+            <h1 className="font-[600] text-[2em]" >Meal Not found 404!</h1>
+        </main>
+    );
+}
--- app/meals/[slug]/page.tsx
+++ app/meals/[slug]/page.tsx
@@ -1,10 +1,35 @@
+import { getMeal } from "@/lib/meals";
 import type { Params } from "next/dist/server/request/params";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
 export default function MealsPost({params}: {params:Params}){ // here there is issue (1)
-    return (
-        <div className="">
-            <h1 className="">Meals Post</h1>
-            <p className="w-[100%] my-[1em] p-[10px] font-[700] rounded-[10px] bg-[tomato]">{ params.slug }</p>{/* // here there is issue (2) */}
-        </div>
-    );
+
+    const slug = params?.slug;
+    if(slug){
+        const meal = getMeal(slug);  
+        if(!meal){
+            notFound();
+        }
+        meal.instructions.replace(/\n/g,'<br/>');
+        return (
+            <>
+                <header className="w-[80%] m-[auto] py-[10px] flex justify-start items-center">
+                    <div className="w-[20em] h-[14em]  relative">
+                        <Image src={meal.image} alt={meal.title} fill  />
+                    </div>
+                    <div className="">
+                        <h1 className="text-[2.3em] font-[700]">{meal.title}</h1>
+                        <p className="text-[1.2em]">by <a className="font-[500] text-[orange]" href={`mailto:${meal.creator_email}`}>{meal.creator}</a></p>
+                        <p className="font-[500]">{meal.summary}</p>
+                    </div>
+                </header>
+                <main className="w-[80%] m-[auto]">
+                    <p className="w-[80%]  m-[auto] font-[700] text-[white] my-[1em] p-[10px] bg-[orange] rounded-[10px] min-h-[3em]" dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }} ></p>
+                </main>
+            </>
+        );
+    }
 }
