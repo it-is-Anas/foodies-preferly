70d779965e2d9236ca4026c6b761d4de9fbc0fee
prontoTheLegend <anasprog8@gmail.com>
2025-09-06 14:26:50 +0300

    saving new meals

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb', // Increase to 10MB or your desired limit
+    },
+  },
 };
 
 export default nextConfig;
